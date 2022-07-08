ba33cc360049ca94531e8529b9e854f145bdb27b
Luis Camargo <luiska1993@gmail.com>
2022-07-08 12:34:06 -0500

    chore: token util

--- src/common/tokens.ts
+++ src/common/tokens.ts
@@ -1,7 +1,5 @@
 /* eslint-disable prefer-const */
-import { ERC20 } from "../../generated/UniswapV2Factory/ERC20";
-import { ERC20SymbolBytes } from "../../generated/UniswapV2Factory/ERC20SymbolBytes";
-import { ERC20NameBytes } from "../../generated/UniswapV2Factory/ERC20NameBytes";
+import { ERC20 } from "../../generated/Deployer/ERC20";
 import { Address } from "@graphprotocol/graph-ts";
 
 export const INVALID_TOKEN_DECIMALS = 9999;
@@ -9,7 +7,6 @@ export const UNKNOWN_TOKEN_VALUE = "unknown";
 
 export function fetchTokenSymbol(tokenAddress: Address): string {
   let contract = ERC20.bind(tokenAddress);
-  let contractSymbolBytes = ERC20SymbolBytes.bind(tokenAddress);
 
   // try types string and bytes32 for symbol
   let symbolValue = UNKNOWN_TOKEN_VALUE;
@@ -19,11 +16,11 @@ export function fetchTokenSymbol(tokenAddress: Address): string {
   }
 
   // non-standard ERC20 implementation
-  let symbolResultBytes = contractSymbolBytes.try_symbol();
-  if (!symbolResultBytes.reverted) {
+  let symbol = contract.try_symbol();
+  if (!symbol.reverted) {
     // for broken pairs that have no symbol function exposed
-    if (!isNullEthValue(symbolResultBytes.value.toHexString())) {
-      symbolValue = symbolResultBytes.value.toString();
+    if (!isNullEthValue(symbol.value)) {
+      symbolValue = symbol.value.toString();
     } else {
       // try with the static definition
       let staticTokenDefinition = StaticTokenDefinition.fromAddress(
@@ -40,7 +37,6 @@ export function fetchTokenSymbol(tokenAddress: Address): string {
 
 export function fetchTokenName(tokenAddress: Address): string {
   let contract = ERC20.bind(tokenAddress);
-  let contractNameBytes = ERC20NameBytes.bind(tokenAddress);
 
   // try types string and bytes32 for name
   let nameValue = UNKNOWN_TOKEN_VALUE;
@@ -50,10 +46,10 @@ export function fetchTokenName(tokenAddress: Address): string {
   }
 
   // non-standard ERC20 implementation
-  let nameResultBytes = contractNameBytes.try_name();
+  let nameResultBytes = contract.try_name();
   if (!nameResultBytes.reverted) {
     // for broken exchanges that have no name function exposed
-    if (!isNullEthValue(nameResultBytes.value.toHexString())) {
+    if (!isNullEthValue(nameResultBytes.value)) {
       nameValue = nameResultBytes.value.toString();
     } else {
       // try with the static definition
@@ -103,6 +99,7 @@ class StaticTokenDefinition {
   decimals: i32;
 
   // Initialize a Token Definition with its attributes
+  // TODO: Yield tokens? only needed if we want to track the baskets
   constructor(address: Address, symbol: string, name: string, decimals: i32) {
     this.address = address;
     this.symbol = symbol;
@@ -114,59 +111,22 @@ class StaticTokenDefinition {
   static getStaticDefinitions(): Array<StaticTokenDefinition> {
     let staticDefinitions = new Array<StaticTokenDefinition>(6);
 
-    // Add DGD
-    let tokenDGD = new StaticTokenDefinition(
-      Address.fromString("0xe0b7927c4af23765cb51314a0e0521a9645f0e2a"),
-      "DGD",
-      "DGD",
-      9 as i32
-    );
-    staticDefinitions.push(tokenDGD);
-
-    // Add AAVE
-    let tokenAAVE = new StaticTokenDefinition(
-      Address.fromString("0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"),
-      "AAVE",
-      "Aave Token",
+    // Add RSR
+    let tokenRSR = new StaticTokenDefinition(
+      Address.fromString("0x320623b8e4ff03373931769a31fc52a4e78b5d70"),
+      "RSR",
+      "Reserve Rights",
       18 as i32
     );
-    staticDefinitions.push(tokenAAVE);
+    staticDefinitions.push(tokenRSR);
 
-    // Add LIF
-    let tokenLIF = new StaticTokenDefinition(
-      Address.fromString("0xeb9951021698b42e4399f9cbb6267aa35f82d59d"),
-      "LIF",
-      "Lif",
+    // Add RSV
+    let tokenRSV = new StaticTokenDefinition(
+      Address.fromString("0x196f4727526eA7FB1e17b2071B3d8eAA38486988"),
+      "RSV",
+      "Reserve",
       18 as i32
     );
-    staticDefinitions.push(tokenLIF);
-
-    // Add SVD
-    let tokenSVD = new StaticTokenDefinition(
-      Address.fromString("0xbdeb4b83251fb146687fa19d1c660f99411eefe3"),
-      "SVD",
-      "savedroid",
-      18 as i32
-    );
-    staticDefinitions.push(tokenSVD);
-
-    // Add TheDAO
-    let tokenTheDAO = new StaticTokenDefinition(
-      Address.fromString("0xbb9bc244d798123fde783fcc1c72d3bb8c189413"),
-      "TheDAO",
-      "TheDAO",
-      16 as i32
-    );
-    staticDefinitions.push(tokenTheDAO);
-
-    // Add HPB
-    let tokenHPB = new StaticTokenDefinition(
-      Address.fromString("0x38c6a68304cdefb9bec48bbfaaba5c5b47818bb2"),
-      "HPB",
-      "HPBCoin",
-      18 as i32
-    );
-    staticDefinitions.push(tokenHPB);
 
     return staticDefinitions;
   }
